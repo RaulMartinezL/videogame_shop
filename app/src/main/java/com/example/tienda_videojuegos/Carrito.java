b63d94cca65cd1817b07c439766499af1cb5ef5c
Raul Martinez <raulml2527@hotmail.com>
2020-05-06 23:02:59 +0100

    añadido primera parte del carrito de la compra

---
+++ app/src/main/java/com/example/tienda_videojuegos/Carrito.java
@@ -0,0 +1,37 @@
+package com.example.tienda_videojuegos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.text.Layout;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Carrito extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+
+    private Button NextProcessBtn;
+    private TextView txtTotalAmount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carrito);
+
+
+        recyclerView = findViewById(R.id.cat_list);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        NextProcessBtn = (Button) findViewById(R.id.next_process_btn);
+        txtTotalAmount = (TextView) findViewById(R.id.total_price);
+
+
+
+    }
+}
