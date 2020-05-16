c6bdc8037935a0cce402556481a02c5d881b6560
Raul Martinez <raulml2527@hotmail.com>
2020-05-16 22:11:46 +0200

    ultimos cambios

--- app/src/main/java/com/example/tienda_videojuegos/MapsActivity.java
+++ app/src/main/java/com/example/tienda_videojuegos/MapsActivity.java
@@ -4,14 +4,19 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -34,89 +39,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-
-        toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigationView);
-
-
-
-
-        final ActionBar actionbar = getSupportActionBar();
-        actionbar.setDisplayHomeAsUpEnabled(true);
-        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                switch (item.getItemId())
-                {
-                    case R.id.nav_ps4:
-                        item.setChecked(true);
-                        Intent intentPS4 = new Intent(MapsActivity.this, PS4Games.class);
-                        startActivity(intentPS4);
-                        drawerLayout.closeDrawers();
-                        return true;
-
-                    case R.id.nav_xbox:
-                        item.setChecked(true);
-                        Intent intentXbox = new Intent(MapsActivity.this, XboxGames.class);
-                        startActivity(intentXbox);
-                        drawerLayout.closeDrawers();
-                        return true;
-
-                    case R.id.nav_ofertas:
-                        item.setChecked(true);
-                        Intent intentOfertas = new Intent(MapsActivity.this, OfertasGames.class);
-                        startActivity(intentOfertas);
-                        drawerLayout.closeDrawers();
-                        return true;
-
-                    case R.id.nav_novedades:
-                        item.setChecked(true);
-                        Intent intentNovedades = new Intent(MapsActivity.this, NovedadesGames.class);
-                        startActivity(intentNovedades);
-                        drawerLayout.closeDrawers();
-                        return true;
-                    case R.id.nav_contacto:
-                        item.setChecked(true);
-                        Intent intentContacto = new Intent(MapsActivity.this, ContactoSelect.class);
-                        startActivity(intentContacto);
-                        drawerLayout.closeDrawers();
-                        return true;
-
-                    case R.id.nav_donde_estamos:
-                        item.setChecked(true);
-                        Intent intentGPS = new Intent(MapsActivity.this, MapsActivity.class);
-                        startActivity(intentGPS);
-                        drawerLayout.closeDrawers();
-                        return true;
-                }
-                return false;
-            }
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -144,25 +66,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         map.addMarker(new MarkerOptions().position(localizacin_tienda).title("Estamos aquí!!"));
         map.moveCamera(CameraUpdateFactory.newLatLng(localizacin_tienda));
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId())
-        {
-            case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
 }
